8087bf0b163e67023e35921e1fef1b9b90f4a8f9
AdiJiyer <70841250+AdiJiyer@users.noreply.github.com>
2021-01-17 18:23:01 +0530

    Update common.js

--- scripts/common.js
+++ scripts/common.js
@@ -1,20 +1,20 @@
-
-  function showSignInModal(){
-    var modal = document.getElementById("signin-modal");
-    modal.style.display = "flex";
-  }
+// Below code no longer used as trasitions provided by bootstrap
+//   function showSignInModal(){
+//     var modal = document.getElementById("signin-modal");
+//     modal.style.display = "flex";
+//   }
   
-  function hideSignInModal(){
-    var modal = document.getElementById("signin-modal");
-    modal.style.display = "none";
-  }
+//   function hideSignInModal(){
+//     var modal = document.getElementById("signin-modal");
+//     modal.style.display = "none";
+//   }
   
-  function showSignUpModal(){
-    var modal = document.getElementById("signup-modal");
-    modal.style.display = "flex";
-  }
+//   function showSignUpModal(){
+//     var modal = document.getElementById("signup-modal");
+//     modal.style.display = "flex";
+//   }
   
-  function hideSignUpModal(){
-    var modal = document.getElementById("signup-modal");
-    modal.style.display = "none";
-  }
+//   function hideSignUpModal(){
+//     var modal = document.getElementById("signup-modal");
+//     modal.style.display = "none";
+//   }
