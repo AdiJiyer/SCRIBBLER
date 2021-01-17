41d2ae2af963e56dceb922f96676a898dea75b6c
AdiJiyer <70841250+AdiJiyer@users.noreply.github.com>
2021-01-17 18:19:59 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,14 @@
-function showCreatePost(){
-  var modal = document.getElementById("create-post-modal");
-  modal.style.display = "flex";
-}
+// trasition created with bootstrap instead of js below code no longer used
+// function showCreatePost(){
+//   var modal = document.getElementById("create-post-modal");
+//   modal.style.display = "flex";
+// }
 
-function hideCreatePostModal(){
-  var modal = document.getElementById("create-post-modal");
-  modal.style.display = "none";
-}
-  function navigateToBlogList(){
-    location.href = "./html/postslist.html"
-  }
+// function hideCreatePostModal(){
+//   var modal = document.getElementById("create-post-modal");
+//   modal.style.display = "none";
+// }
+//   function navigateToBlogList(){
+//     location.href = "./html/postslist.html"
+//   }
   
